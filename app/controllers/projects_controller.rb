af7b181355601b46efd7cbc6c473b2e572e3ae73
Andreas Minnich <andreas.minnich@gmail.com>
2015-03-18 15:35:42 +0100

    - nest routes max 1 level - render projects index/show with json

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,11 @@
 class ProjectsController < ApplicationController
+  def index
+    @projects = Project.all
+    render json: @projects, include: ['scrum_master', 'product_owner']
+  end
+
+  def show
+    @project = Project.find(params[:id])
+    render json: @project, include: ['scrum_master', 'product_owner', 'sprints']
+  end
 end
